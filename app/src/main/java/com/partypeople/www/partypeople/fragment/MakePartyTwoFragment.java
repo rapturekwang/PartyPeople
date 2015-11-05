6150e63d1c14701162467e08bdbda9fa78a5d64e
Kwanghee Jeong <rapturekwang@gmail.com>
2015-11-05 18:37:33 +0900

    make party second page done

--- app/src/main/java/com/partypeople/www/partypeople/fragment/MakePartyTwoFragment.java
+++ app/src/main/java/com/partypeople/www/partypeople/fragment/MakePartyTwoFragment.java
@@ -5,17 +5,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.partypeople.www.partypeople.R;
 import com.partypeople.www.partypeople.activity.MakePartyActivity;
+import com.partypeople.www.partypeople.utils.Constants;
 
 /**
  * Created by Tacademy on 2015-10-29.
  */
 public class MakePartyTwoFragment extends Fragment {
     private static final String ARG_NAME = "name";
+    ArrayAdapter<String> mYearAdapter, mMonthAdapter, mDayAdapter;
 
     // TODO: Rename and change types of parameters
     private String name;
@@ -58,6 +62,74 @@ public class MakePartyTwoFragment extends Fragment {
             }
         });
 
+        setDateSpinner(view);
+
         return view;
+    }
+
+    private void setDateSpinner(View view) {
+        Spinner spinner = (Spinner)view.findViewById(R.id.spinner_year);
+        mYearAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item);
+        mYearAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(mYearAdapter);
+//        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                Toast.makeText(getContext(), "position : " + position, Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        });
+
+        spinner = (Spinner)view.findViewById(R.id.spinner_month);
+        mMonthAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item);
+        mMonthAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(mMonthAdapter);
+//        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                Toast.makeText(getContext(), "position : " + position, Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        });
+
+        spinner = (Spinner)view.findViewById(R.id.spinner_day);
+        mDayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item);
+        mDayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(mDayAdapter);
+//        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                Toast.makeText(getContext(), "position : " + position, Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        });
+
+        mYearAdapter.add("년");
+        int num = 2015;
+        for (int i = num;i<num+ Constants.MAX_YEAR; i++) {
+            mYearAdapter.add(""+i);
+        }
+        mMonthAdapter.add("월");
+        num = 1;
+        for (int i = num; i<num+Constants.NUM_OF_MONTH; i++) {
+            mMonthAdapter.add(""+i);
+        }
+        mDayAdapter.add("일");
+        num = 1;
+        for (int i = num; i<num+Constants.NUM_OF_DAY; i++) {
+            mDayAdapter.add(""+i);
+        }
     }
 }
