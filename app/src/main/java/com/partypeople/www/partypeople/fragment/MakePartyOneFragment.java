3dde4ae032dd61d9152b4b04ed7f576f8bb87674
Kwanghee Jeong <rapturekwang@gmail.com>
2015-11-05 13:39:35 +0900

    make party add image crop

--- app/src/main/java/com/partypeople/www/partypeople/fragment/MakePartyOneFragment.java
+++ app/src/main/java/com/partypeople/www/partypeople/fragment/MakePartyOneFragment.java
@@ -1,16 +1,26 @@
 package com.partypeople.www.partypeople.fragment;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.partypeople.www.partypeople.R;
 import com.partypeople.www.partypeople.activity.MakePartyActivity;
+
+import java.io.File;
 
 /**
  * Created by Tacademy on 2015-10-29.
@@ -20,7 +30,9 @@ public class MakePartyOneFragment extends Fragment {
 
     // TODO: Rename and change types of parameters
     private String name;
-
+    File mSavedFile;
+    public static final int REQUEST_CODE_CROP = 0;
+    ImageView imageView;
 
     public static MakePartyOneFragment newInstance(String name) {
         MakePartyOneFragment fragment = new MakePartyOneFragment();
@@ -42,6 +54,12 @@ public class MakePartyOneFragment extends Fragment {
         if (getArguments() != null) {
             name = getArguments().getString(ARG_NAME);
         }
+        if (savedInstanceState != null) {
+            String file = savedInstanceState.getString("filename");
+            if (file != null) {
+                mSavedFile = new File(file);
+            }
+        }
     }
 
     @Override
@@ -50,16 +68,56 @@ public class MakePartyOneFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_make_party_one, container, false);
 
+        imageView = (ImageView)view.findViewById(R.id.image_make_party);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoPickerIntent = new Intent(
+                        Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                photoPickerIntent.setType("image/*");
+                photoPickerIntent.putExtra("crop", "true");
+                photoPickerIntent.putExtra(MediaStore.EXTRA_OUTPUT, getTempUri());
+                photoPickerIntent.putExtra("outputFormat",
+                        Bitmap.CompressFormat.JPEG.toString());
+                startActivityForResult(photoPickerIntent, REQUEST_CODE_CROP);
+            }
+        });
+
         Button btn = (Button)view.findViewById(R.id.btn_next);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MakePartyActivity activity = (MakePartyActivity)getActivity();
+                MakePartyActivity activity = (MakePartyActivity) getActivity();
                 activity.nextFragment();
             }
         });
 //        nameView = (TextView)v.findViewById(R.id.text_name);
 //        nameView.setText(name);
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("test", "test");
+        if (requestCode == REQUEST_CODE_CROP && resultCode == getActivity().RESULT_OK) {
+            Log.d("test", "test123");
+            Bitmap bm = BitmapFactory.decodeFile(mSavedFile.getAbsolutePath());
+            imageView.setImageBitmap(bm);
+        }
+    }
+
+    private Uri getTempUri() {
+        mSavedFile = new File(Environment.getExternalStorageDirectory(),"temp_" + System.currentTimeMillis()/1000);
+
+        return Uri.fromFile(mSavedFile);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (mSavedFile != null) {
+            outState.putString("filename", mSavedFile.getAbsolutePath());
+        }
     }
 }
