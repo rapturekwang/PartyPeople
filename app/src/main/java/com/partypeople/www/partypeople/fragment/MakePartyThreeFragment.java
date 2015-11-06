434e3bd71a2830276bb9c9dc8e431a87ea909da5
Kwanghee Jeong <rapturekwang@gmail.com>
2015-11-06 10:18:36 +0900

    make party done

--- app/src/main/java/com/partypeople/www/partypeople/fragment/MakePartyThreeFragment.java
+++ app/src/main/java/com/partypeople/www/partypeople/fragment/MakePartyThreeFragment.java
@@ -1,5 +1,7 @@
 package com.partypeople.www.partypeople.fragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -7,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.partypeople.www.partypeople.R;
 import com.partypeople.www.partypeople.activity.MakePartyActivity;
@@ -49,12 +52,29 @@ public class MakePartyThreeFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_make_party_three, container, false);
 
-        Button btn = (Button)view.findViewById(R.id.btn_next);
+        Button btn = (Button)view.findViewById(R.id.btn_make);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MakePartyActivity activity = (MakePartyActivity)getActivity();
-                activity.nextFragment();
+//                MakePartyActivity activity = (MakePartyActivity)getActivity();
+//                activity.nextFragment();
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setIcon(android.R.drawable.ic_dialog_alert);
+                builder.setMessage("정말로 만드시겠습니까?");
+                builder.setPositiveButton("만들기", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getContext(), "모임이 생성되었습니다.", Toast.LENGTH_SHORT).show();
+                        getActivity().finish();
+                    }
+                });
+                builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                builder.create().show();
             }
         });
 
