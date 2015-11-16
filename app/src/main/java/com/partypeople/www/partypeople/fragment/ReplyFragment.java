18e8e504a19fa766f431f853fc8c54233cc64c2d
Kwanghee Jeong <rapturekwang@gmail.com>
2015-11-16 14:39:59 +0900

    message page has bug

--- app/src/main/java/com/partypeople/www/partypeople/fragment/ReplyFragment.java
+++ app/src/main/java/com/partypeople/www/partypeople/fragment/ReplyFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.partypeople.www.partypeople.R;
@@ -33,11 +34,13 @@ public class ReplyFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_reply, container, false);
+
+        EditText editText = (EditText)view.findViewById(R.id.editText);
+        editText.setText("");
 
         final MessageActivity activity = (MessageActivity)getActivity();
 
