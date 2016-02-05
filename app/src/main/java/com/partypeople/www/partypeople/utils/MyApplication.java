a15c049ce6fd2347407790f0295cc899b869b3ed
kwang <rapturekwang@gmail.com>
2016-02-05 10:52:34 +0900

    font file change

--- app/src/main/java/com/partypeople/www/partypeople/utils/MyApplication.java
+++ app/src/main/java/com/partypeople/www/partypeople/utils/MyApplication.java
@@ -18,9 +18,9 @@ public class MyApplication extends MultiDexApplication{
         super.onCreate();
         mContext = this;
         FacebookSdk.sdkInitialize(this);
-        FontManager.setDefaultFont(this, "DEFAULT", "Spoqa_Han_Sans_Regular.ttf");
+        FontManager.setDefaultFont(this, "DEFAULT", "SpoqaHanSans-Regular.ttf");
         Typekit.getInstance()
-                .addNormal(Typekit.createFromAsset(this, "Spoqa_Han_Sans_Regular.ttf"))
+                .addNormal(Typekit.createFromAsset(this, "SpoqaHanSans-Regular.ttf"))
                 .addBold(Typekit.createFromAsset(this, "Spoqa_Han_Sans_Bold_win_subset.ttf"));
     }
 
